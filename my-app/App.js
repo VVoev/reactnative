b22457a309d0de34334e16ce52386514b139369b
vladovoev <vladovoev@yahoo.co.uk>
2018-06-12 18:12:28 +0300

    added some components

--- my-app/App.js
+++ my-app/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import ListItem from './src/components/ListItem/ListItem';
 
 export default class App extends React.Component {
+
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  changeTextHandler = (val) => {
+    this.setState({ placeName: val })
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
   render() {
+
+    const placesOutput = this.state.places.map((place, index) => {
+      return <ListItem key={index} placeName={place} />
+    })
     return (
       <View style={styles.container}>
-        <Text>Vlado beshe tuka</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.changeTextHandler}></TextInput>
+          <Button title="Add" onPress={this.placeSubmitHandler} styles={styles.placeButton} />
+        </View>
+        <View styles={styles.listContainer}>{placesOutput}</View>
       </View>
     );
   }
@@ -15,9 +46,23 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: '30%'
+  },
+  listContainer: {
+    width: '100%'
+  }
 });
---
+++ my-app/src/components/ListItem/ListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListItem = (props) => {
+    return (
+        <View style={styles.ListItem}>
+            <Text>{props.placeName}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    ListItem: {
+        width: '100%',
+        padding: 10,
+        backgroundColor: '#eee',
+    }
+})
+
+export default ListItem;
